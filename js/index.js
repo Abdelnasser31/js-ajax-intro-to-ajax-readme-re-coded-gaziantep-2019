4915871085e39b0f0354e23ac115f6fa55252f42
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-21 08:04:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,6 +3,5 @@ const baseUrl = 'https://api.github.com';
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET',`${baseUrl}/users/abdelnasser31/repos`);
-  debugger;
   req.send();
 }
