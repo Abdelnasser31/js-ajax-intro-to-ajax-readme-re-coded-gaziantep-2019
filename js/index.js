b15403f96d3e63dd3144dcf4e140d9fb0ac91c13
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-21 08:56:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,6 +9,15 @@ function getRepositories() {
 function showRepositories() {
   let repos = JSON.parse(this.responseText);
   
-  let repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+   const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
 document.getElementById('repositories').innerHTML = repoList;
 }
